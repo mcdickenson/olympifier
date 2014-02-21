590c10ee91bec2a51199e37986f9c3a6b74c8308
Matt Dickenson <mcdickenson@gmail.com>
2014-02-21 17:32:34 -0500

    update svm

--- svm.rb
+++ svm.rb
@@ -2,6 +2,18 @@ require 'libsvm'
 
 load('data_accessor.rb')
 
+# split data into training and test sets
+athletes  = DataAccessor.new nil, filename="data/athletes.tsv"; nil
+trn_set, tst_set = athletes.two_subsets; nil
+
+sports = trn_set.get_features(:sport).flatten.uniq
+
+trn_examples = trn_set.get_features.map{ |row| Libsvm::Node.features(row) }; nil
+tst_examples = tst_set.get_features.map{ |row| Libsvm::Node.features(row) }; nil
+
+trn_labels  = trn_set.get_features(:sport).flatten.map{|row| sports.index(row)}; nil
+tst_classes = tst_set.get_features(:sport).flatten; nil
+
 # set up problem (boilerplate)
 problem   = Libsvm::Problem.new
 parameter = Libsvm::SvmParameter.new
@@ -10,20 +22,8 @@ parameter.cache_size = 1
 parameter.eps = 0.001
 parameter.c   = 10
 
-# split data into training and test sets
-athletes  = DataAccessor.new nil, filename="data/athletes.tsv"; nil
-train_set = DataAccessor.new athletes[0..999]; nil
-test_set  = DataAccessor.new athletes[1000..1009]; nil
-sports    = train_set.get_features(:sport).flatten.uniq
-
-train_examples = train_set.get_features.map{ |row| Libsvm::Node.features(row) }; nil
-test_examples  = test_set.get_features.map{ |row| Libsvm::Node.features(row) }; nil
-
-train_labels = train_set.get_features(:sport).flatten.map{|row| sports.index(row)}; nil
-test_labels  = test_set.get_features(:sport).flatten; nil
-
 # train model
-problem.set_examples(train_labels, train_examples)
+problem.set_examples(trn_labels, trn_examples)
 model = Libsvm::Model.train(problem, parameter) # takes a little while
 
 # test model 
