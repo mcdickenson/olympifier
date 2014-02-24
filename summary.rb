a2b48f099243555b15c1b590cdb15befd82e12f1
Matt Dickenson <mcdickenson@gmail.com>
2014-02-24 14:47:32 -0500

    add file to collect summary stats

---
+++ summary.rb
@@ -0,0 +1,23 @@
+require 'csv'
+
+load('dtree.rb')
+load('kmeans.rb')
+load('svm.rb')
+
+n = 10 
+
+CSV.open("summary.csv", "w") do |csv|
+  csv << ["method", "time", "accuracy"]
+  ["dtree", "kmeans", "svm"].each do |meth|
+    (1..9).map{|x| x.to_f/10} do |p|
+      cmd = "#{meth}(#{p})"
+      puts cmd
+      n.times do 
+        start = Time.now
+        acc = eval(cmd)
+        t = Time.now - start
+        csv << [meth, t, acc]
+      end
+    end
+  end
+end
