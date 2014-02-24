4acaab8603a10cfba063d7e4537b1dc2eeece9f5
Matt Dickenson <mcdickenson@gmail.com>
2014-02-24 11:03:21 -0500

    clean up kmeans example

--- kmeans.rb
+++ kmeans.rb
@@ -5,9 +5,6 @@ load('data_accessor.rb')
 athletes  = DataAccessor.new nil, filename="data/athletes.tsv"; nil
 trn_set, tst_set = athletes.two_subsets; nil
 
-# fms = trn_set.feature_means_by(:sport)
-# sports = fms.keys
-# fcs = sports.each_with_object([]){ |k, ary| ary << [fms[k][:height_cm], fms[k][:weight]]}
 sports = trn_set.get_features(:sport).flatten.uniq
 
 trn_examples = trn_set.get_features; nil
@@ -16,6 +13,7 @@ tst_examples = tst_set.get_features; nil
 trn_kmeans = KMeans.new(trn_examples, centroids: sports.length)
 tst_kmeans = KMeans.new(tst_examples, custom_centroids: trn_kmeans.centroids)
 
+# check clusterfications
 tst_assignments = tst_kmeans.instance_variable_get :@centroid_pockets
 tst_assignments.each_with_index do |asn, i|
   if asn.empty?
@@ -30,20 +28,3 @@ tst_assignments.each_with_index do |asn, i|
     end
   end
 end; nil
-
-
-# tst_kmeans.nodes.first
-# tst_kmeans.centroids.first
-
-# todo: see whether each tst_set sport is in trn_set_sports
-
-
-
-
-
-# todo: find the modal sport for each cluster
-
-
-# kmeans = KMeans.new(trn_examples, centroids: sports.length)
-
-
