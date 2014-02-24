bc0fa073e4ef522aaf4413a3d5f3859e0854809a
Matt Dickenson <mcdickenson@gmail.com>
2014-02-24 13:54:37 -0500

    move classifiers into methods

--- kmeans.rb
+++ kmeans.rb
@@ -1,33 +1,32 @@
 require 'k_means'
-
 load('data_accessor.rb')
 
-athletes  = DataAccessor.new nil, filename="data/athletes.tsv"; nil
-trn_set, tst_set = athletes.two_subsets; nil
+def kmeans(percent)
+  athletes  = DataAccessor.new nil, filename="data/athletes.tsv"; nil
+  trn_set, tst_set = athletes.two_subsets; nil
 
-sports = trn_set.get_features(:sport).flatten.uniq
+  sports = trn_set.get_features(:sport).flatten.uniq
 
-trn_examples = trn_set.get_features; nil
-tst_examples = tst_set.get_features; nil
+  trn_examples = trn_set.get_features; nil
+  tst_examples = tst_set.get_features; nil
 
-start = Time.now
-trn_kmeans = KMeans.new(trn_examples, centroids: sports.length)
-puts "training kmeans on #{trn_set.length} examples took #{Time.now-start} seconds"
+  start = Time.now
+  trn_kmeans = KMeans.new(trn_examples, centroids: sports.length)
+  t = Time.now-start
 
-tst_kmeans = KMeans.new(tst_examples, custom_centroids: trn_kmeans.centroids)
+  tst_kmeans = KMeans.new(tst_examples, custom_centroids: trn_kmeans.centroids)
+  tst_assignments = tst_kmeans.instance_variable_get :@centroid_pockets
 
+  tst_classes = []
+  pred_labels = []
 
-tst_assignments = tst_kmeans.instance_variable_get :@centroid_pockets
+  tst_assignments.each_with_index do |asn, i|
+    asn.each do |j|
+      pred_labels << sports[i]
+      tst_classes << tst_set[j][:sport]
+    end
+  end; nil
+  acc = tst_set.check_accuracy(pred_labels, tst_classes)
 
-tst_classes = []
-pred_labels = []
-
-tst_assignments.each_with_index do |asn, i|
-  asn.each do |j|
-    pred_labels << sports[i]
-    tst_classes << tst_set[j][:sport]
-  end
-end; nil
-acc = tst_set.check_accuracy(pred_labels, tst_classes)
-
-puts "kmeans accuracy rate on #{tst_set.length} examples is #{acc}"
+  {time: t, accuracy: acc}
+end
--- svm.rb
+++ svm.rb
@@ -1,39 +1,35 @@
 require 'libsvm'
-
 load('data_accessor.rb')
 
-# split data into training and test sets
-athletes  = DataAccessor.new nil, filename="data/athletes.tsv"; nil
-trn_set, tst_set = athletes.two_subsets(percent_in_first_subset=0.05); nil
+def svm(percent)
+  # split data into training and test sets
+  athletes  = DataAccessor.new nil, filename="data/athletes.tsv"; nil
+  trn_set, tst_set = athletes.two_subsets(percent_in_first_subset=0.05); nil
 
-sports = trn_set.get_features(:sport).flatten.uniq
+  sports = trn_set.get_features(:sport).flatten.uniq
 
-trn_examples = trn_set.get_features.map{ |row| Libsvm::Node.features(row) }; nil
-tst_examples = tst_set.get_features.map{ |row| Libsvm::Node.features(row) }; nil
+  trn_examples = trn_set.get_features.map{ |row| Libsvm::Node.features(row) }; nil
+  tst_examples = tst_set.get_features.map{ |row| Libsvm::Node.features(row) }; nil
 
-trn_labels  = trn_set.get_features(:sport).flatten.map{|row| sports.index(row)}; nil
-tst_classes = tst_set.get_features(:sport).flatten; nil
+  trn_labels  = trn_set.get_features(:sport).flatten.map{|row| sports.index(row)}; nil
+  tst_classes = tst_set.get_features(:sport).flatten; nil
 
-# set up problem (boilerplate)
-problem   = Libsvm::Problem.new
-parameter = Libsvm::SvmParameter.new
+  problem   = Libsvm::Problem.new
+  parameter = Libsvm::SvmParameter.new
+  parameter.cache_size = 5
+  parameter.eps = 0.01
+  parameter.c   = 10
 
-parameter.cache_size = 5
-parameter.eps = 0.01
-parameter.c   = 10
+  # train model
+  problem.set_examples(trn_labels, trn_examples)
+  start = Time.now
+  model = Libsvm::Model.train(problem, parameter) # takes a little while
+  t = Time.now-start
 
-# train model
-problem.set_examples(trn_labels, trn_examples)
+  # test model 
+  pred = tst_examples.map {|x| model.predict(x) }
+  pred_labels = pred.map{|x| sports[x]}
 
-start = Time.now
-model = Libsvm::Model.train(problem, parameter) # takes a little while
-puts "training svm on #{trn_set.length} examples took #{Time.now-start} seconds"
-
-# test model 
-pred = tst_examples.map {|x| model.predict(x) }
-pred_labels = pred.map{|x| sports[x]}
-
-acc = tst_set.check_accuracy(pred_labels, tst_classes)
-
-puts "svm accuracy rate on #{tst_set.length} examples is #{acc}"
-
+  acc = tst_set.check_accuracy(pred_labels, tst_classes)
+  {time: t, accuracy: acc}
+end
