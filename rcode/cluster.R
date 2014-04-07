1d192f4c9934b2c56a27d085cf5e98d81d8732c1
Matt Dickenson <mcdickenson@gmail.com>
2014-04-06 22:44:52 -0400

    first run of hierarchical clustering

---
+++ rcode/cluster.R
@@ -0,0 +1,31 @@
+# set up workspace
+setwd('~/github/olympifier')
+library(mclust)
+
+# load data
+athletes = read.csv('data/athletes-mod.csv', header=TRUE, as.is=TRUE)
+dim(athletes)
+head(athletes)
+
+# set features and target
+features = c("Age", "Height", "Weight")
+target = "Sport"
+# target = "Event"
+
+
+# check model
+# ?Mclust
+# athletesBIC <- mclustBIC(athletes)
+# mclust = Mclust(athletes[, features], G=3)
+# mclust = Mclust(athletes[, features], modelNames=c("VVV"))
+# summary(mclust) # optimal model according to BIC
+
+
+hcVVVathletes = hc(modelName="VVV", data=athletes[, features])
+
+
+# hcVVViris <- hc(modelName = "VVV", data = iris[,-5])
+# cl <- hclass(hcVVViris, 2:3)
+# classError(cl[,"1"], truth = iris[,5])
+# classError(cl[,"2"], truth = iris[,5])
+# classError(cl[,"3"], truth = iris[,5])
